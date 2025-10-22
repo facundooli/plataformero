f36acd80de3ced55514f1615f2592ce6036fbaef
facundooli <facundo.oli81@gmail.com>
2025-10-22 09:54:33 -0300

    agregar monedas y sumar puntaje

---
+++ plataformero/nivel.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var puntaje = 0 
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_moneda_body_entered(body: Node2D) -> void:
+	puntaje = puntaje + 1
+	print(puntaje)
